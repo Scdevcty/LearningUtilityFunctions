be5e204601e3e7d8ecf6cedbc8441d837ff08489
Florian Richoux <florian.richoux@polytechnique.edu>
2021-02-24 20:24:03 +0900

    Add EO libs for mac

--- learn/compile_learn_ga.sh
+++ learn/compile_learn_ga.sh
@@ -4,9 +4,9 @@ OS="$(uname)"
 
 if [ "$OS" == "Darwin" ]; then
 		export CXX=/usr/local/bin/g++-10
-		$CXX -std=c++14 -O3 learn_cf_ga.cpp function_to_learn_icn.cpp print.cpp -Iinclude -L/usr/local/lib -leo -leoutils -o bin/learn_cf_ga
+		$CXX -std=c++14 -O3 learn_cf_ga.cpp function_to_learn_icn.cpp print.cpp -Iinclude -Llib -leo_osx -leoutils_osx -o bin/learn_cf_ga
 fi
 
 if [ "$OS" == "Linux" ]; then
-		g++ -O3 learn_cf_ga.cpp function_to_learn_icn.cpp print.cpp -Iinclude -Llib -leo -leoutils -o bin/learn_cf_ga
+		g++ -O3 learn_cf_ga.cpp function_to_learn_icn.cpp print.cpp -Iinclude -Llib -leo_linux -leoutils_linux -o bin/learn_cf_ga
 fi
